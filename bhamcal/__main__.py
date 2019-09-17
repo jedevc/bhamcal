440d96499771fc6f910d6f53b0e5fc0eac13bb81
Justin Chadwell <jedevc@gmail.com>
2019-09-17 21:44:48 +0100

    Move main method into bhamcal.py

--- bhamcal/__main__.py
+++ bhamcal/__main__.py
@@ -1,19 +1,4 @@
-import click
-
-from .frame import Frame
-from .extractor import extract
-
-@click.command("bhamcal")
-@click.argument('username')
-@click.option('-o', '--output', default='-', type=click.File('w'))
-@click.password_option()
-def main(username, password, output):
-    fr = Frame(username, password)
-    source = fr.extract(fr.CHROME)
-
-    calendar = extract(source)
-
-    output.write(calendar)
+from .bhamcal import main
 
 if __name__ == "__main__":
     main()
---
+++ bhamcal/bhamcal.py
@@ -0,0 +1,16 @@
+import click
+
+from .frame import Frame
+from .extractor import extract
+
+@click.command("bhamcal")
+@click.argument('username')
+@click.option('-o', '--output', default='-', type=click.File('w'))
+@click.password_option()
+def main(username, password, output):
+    fr = Frame(username, password)
+    source = fr.extract(fr.CHROME)
+
+    calendar = extract(source)
+
+    output.write(calendar)
