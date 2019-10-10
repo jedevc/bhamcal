bed76fff31bc285be1af8c608f75ccef10e8da56
Charles de Freitas <charles@defreitas.io>
2019-10-10 10:27:35 +0100

    Better handling for double names. (#6)
    * Fix duplicate name parsing.
    
    * Add handling for intended `/`
    
    ---
    C/C++ -> C/C++
    
    Networks /Networks -> Networks
    ---

--- bhamcal/extractor.py
+++ bhamcal/extractor.py
@@ -76,11 +76,14 @@ CODE_STRIPPER  = re.compile(
 )
 # Remove duplicates in the case of the name being present twice on some extended modules.
 REMOVE_DOUBLES = re.compile(
-    r".*/(?P<one>^.*)"
+    r"(?P<one>^.*/)"
 )
 
 def clean_subject(subject: str) -> str:
     subject = re.sub(CODE_STRIPPER, "", subject)
-    subject = re.sub(REMOVE_DOUBLES, "", subject)
+    items = subject.split("/")
+    if len(items) == 2:
+        if items[0].lower().strip() == items[1].lower().strip():
+            subject = re.sub(REMOVE_DOUBLES, "", subject)
 
     return subject.strip()
